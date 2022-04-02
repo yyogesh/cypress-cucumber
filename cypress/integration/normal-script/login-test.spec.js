d48529e3537023958d10d7e388b4cdd131b3f8d0
yyogesh <yogeshggn1@gmail.com>
2022-04-02 16:13:25 +0530

    cypress basic application

--- cypress/integration/normal-script/login-test.spec.js
+++ cypress/integration/normal-script/login-test.spec.js
@@ -1,5 +1,9 @@
 describe('Login test', () => {
     it('Success Login', () => {
-        cy.visit('/')
+        cy.visit('/');
+        cy.get('#user-name').type('standard_user');
+        cy.get('#password').type('secret_sauce');
+        cy.get('#login-button').click();
+        cy.url().should('contains', '/inventory.html');
     });
 })
