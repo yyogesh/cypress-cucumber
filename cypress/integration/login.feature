511575c413e3c948fb24dc45e4b0088f38d058a6
yyogesh <yogeshggn1@gmail.com>
2022-04-02 17:20:31 +0530

    Page object modal

--- cypress/integration/login.feature
+++ cypress/integration/login.feature
@@ -7,3 +7,11 @@ Feature: Login Feature
         And A user enter the password "secret_sauce"
         And A user clicks on the login button
         Then A user will be logged in
+
+
+    Scenario: Failed Login
+        Given A user opens the login page
+        When A user enter the username "locked_out_user"
+        And A user enter the password "secret_sauce"
+        And A user clicks on the login button
+        Then A user will be receiving a failed message
--- cypress/integration/login/login.js
+++ cypress/integration/login/login.js
@@ -1,21 +1,28 @@
 import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+import { HomeSaucePage } from "../../pages/saucedemo/homeSaucePage";
+
+const homeSaucePage = new HomeSaucePage
 
 Given('A user opens the login page', () => {
     cy.visit('/');
 });
 
 When('A user enter the username {string}', (username) => {
-    cy.get('#user-name').type(username);
+    homeSaucePage.typeUsername(username);
 });
 
 And('A user enter the password {string}', (password) => {
-    cy.get('#password').type(password);
+    homeSaucePage.typePassword(password);
 });
 
 And('A user clicks on the login button', () => {
-    cy.get('#login-button').click();
+    homeSaucePage.clickLogin();
 });
 
 Then('A user will be logged in', () => {
     cy.url().should('contains', '/inventory.html');
 });
+
+Then('A user will be receiving a failed message', () => {
+    homeSaucePage.elements.errorMessage().should('have.text', 'Epic sadface: Sorry, this user has been locked out.')
+})
